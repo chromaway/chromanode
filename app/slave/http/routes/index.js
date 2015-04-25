74c1916ef2f7a065e560440729a46a4580233c4e
Kirill Fomichev <fanatid@ya.ru>
2015-04-25 22:29:52 +0300

    move v1 to v2

--- app/slave/http/routes/index.js
+++ app/slave/http/routes/index.js
@@ -5,7 +5,7 @@ var node = require('../controllers/node')
 module.exports.createRouter = function () {
   var router = express.Router()
 
-  router.use('/v1', require('./v1').createRouter())
+  router.use('/v2', require('./v2').createRouter())
   router.use('/version', node.version)
 
   return router
